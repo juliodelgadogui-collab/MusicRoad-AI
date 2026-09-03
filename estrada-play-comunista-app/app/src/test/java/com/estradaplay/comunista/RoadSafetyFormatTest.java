e14a4e0ef80d72dc706b1511eadc45992b397cd8
juliobji6-ship-it <julio.bji6@gmail.com>
2026-09-03 10:40:43 -0300

    Add road safety format unit tests

---
+++ estrada-play-comunista-app/app/src/test/java/com/estradaplay/comunista/RoadSafetyFormatTest.java
@@ -0,0 +1,22 @@
+package com.estradaplay.comunista;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoadSafetyFormatTest {
+    @Test public void textRoundsMetersForCockpit() {
+        assertEquals("10 m", RoadSafetyFormat.distanceText(4));
+        assertEquals("260 m", RoadSafetyFormat.distanceText(257));
+    }
+
+    @Test public void textUsesKilometersAtOneKm() {
+        assertTrue(RoadSafetyFormat.distanceText(1250).contains("1"));
+        assertTrue(RoadSafetyFormat.distanceText(1250).contains("km"));
+    }
+
+    @Test public void speechKeepsMinimumThirtyMeters() {
+        assertEquals("30 metros", RoadSafetyFormat.distanceSpeech(4));
+        assertEquals("100 metros", RoadSafetyFormat.distanceSpeech(130));
+    }
+}
